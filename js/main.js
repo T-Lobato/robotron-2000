e811a3c3d5465e5a5546b299f54f29204184bf72
Thyago Lobato <thyagollobato@gmail.com>
2023-03-24 08:55:53 -0300

    Montando uma peça

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento);
-});
+const controle = document.querySelectorAll(".controle-ajuste")
 
-function dizOi(nome) {
-    console.log("Olá " + nome);
-    console.log("Bem vindo ao robotron 2000");
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
-
-dizOi();
